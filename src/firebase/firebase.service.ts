a7a653d88d66df8f9bf3bb996ee6d38f2b5ae77d
Pattadon Baongern <pattadonb8@hotmail.com>
2024-08-30 17:23:11 +0700

    setup firebase config

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -8,8 +8,20 @@ export class FirebaseService {
 
   constructor(private readonly configService: ConfigService) {
     const firebaseConfig = {
+      type: this.configService.get<string>('FIREBASE_TYPE'),
       projectId: this.configService.get<string>('FIREBASE_PROJECT_ID'),
       privateKey: this.configService.get<string>('FIREBASE_PRIVATE_KEY'),
+      privateKeyId: this.configService.get<string>('FIREBASE_PRIVATE_KEY_ID'),
+      clientEmail: this.configService.get<string>('FIREBASE_CLIENT_EMAIL'),
+      clientId: this.configService.get<string>('FIREBASE_CLIENT_ID'),
+      authUri: this.configService.get<string>('FIREBASE_AUTH_URI'),
+      tokenUri: this.configService.get<string>('FIREBASE_TOKEN_URI'),
+      authProviderX509CertUrl: this.configService.get<string>(
+        'FIREBASE_AUTH_PROVIDER_X509_CERT_URL',
+      ),
+      clientX509CertUrl: this.configService.get<string>(
+        'FIREBASE_CLIENT_X509_CERT_URL',
+      ),
     }
 
     this.firebaseApp = admin.initializeApp({
